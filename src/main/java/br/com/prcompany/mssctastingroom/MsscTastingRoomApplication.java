b8b5de34103b7cf1ca10b068048cbc7c1e84e052
Paulo Roberto <paulorobertosp@gmail.com>
2021-05-24 09:25:30 -0300

    Initial project

---
+++ src/main/java/br/com/prcompany/mssctastingroom/MsscTastingRoomApplication.java
@@ -0,0 +1,13 @@
+package br.com.prcompany.mssctastingroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscTastingRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscTastingRoomApplication.class, args);
+	}
+
+}
